31ea4321d73d1e9f7c08e650bbb950ea2e2a2db6
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-01-21 16:26:54 +0300

    no more create all on app start

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ from fastapi import FastAPI
 import uvicorn
 
 from core.config import settings
-from core.models import Base, db_helper
 from api_v1 import router as router_v1
 from dishes.views import router as dishes_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
     yield
+
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
