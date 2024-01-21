13b486fe1d69a599717171006d529dfa45ad2c1c
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-01-21 16:44:38 +0300

    upgrade models Dish and Submenu, create migration

--- core/models/dish.py
+++ core/models/dish.py
@@ -1,10 +1,15 @@
-from sqlalchemy import Float
+import uuid
+
+from sqlalchemy import ForeignKey, Float
 from sqlalchemy.orm import Mapped, mapped_column
 
-from .base import Base
+from core.models import Base
 
 
 class Dish(Base):
     __tablename__ = "dishes"
 
     price: Mapped[float] = mapped_column(Float(precision=2))
+    submenu_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("submenus.id"),
+    )
--- core/models/submenu.py
+++ core/models/submenu.py
@@ -1,5 +1,14 @@
+import uuid
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
 from core.models import Base
 
 
 class Submenu(Base):
     __tablename__ = "submenus"
+
+    menu_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("menus.id"),
+    )
