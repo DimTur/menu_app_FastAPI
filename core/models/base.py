2b430e0c103f8841603ff0ac1ed8598ab93c3e08
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-01-25 09:19:28 +0300

    refactor base.py

--- core/models/base.py
+++ core/models/base.py
@@ -1,22 +1,24 @@
 import uuid
 
 from sqlalchemy import String, Text
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr, relationship
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    declared_attr,
+)
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    #создает имя таблицы на основе имени текущего класса
+    # создает имя таблицы на основе имени текущего класса
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
 
     id: Mapped[uuid.UUID] = mapped_column(
-        primary_key=True,
-        default=uuid.uuid4,
-        unique=True,
-        nullable=False
+        primary_key=True, default=uuid.uuid4, unique=True, nullable=False
     )
     title: Mapped[str] = mapped_column(
         String(32),
