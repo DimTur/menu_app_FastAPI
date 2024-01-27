bc33d3a807546e5e24c6ccce67d0c35f08e852fd
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-01-27 18:48:19 +0300

    add test_update_menu_partial

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -13,6 +13,14 @@ async def post_menu() -> dict[str, str]:
     }
 
 
+@pytest.fixture
+async def update_menu() -> dict[str, str]:
+    return {
+        "title": "NEW MENU",
+        "description": "NEW NEW NEW NEW NEW",
+    }
+
+
 @pytest.fixture(scope="module")
 def saved_data() -> dict[str, Any]:
     return {}
--- tests/test_menu.py
+++ tests/test_menu.py
@@ -4,7 +4,11 @@ import pytest
 from httpx import AsyncClient
 
 from conftest import async_client
-from fixtures import post_menu, saved_data
+from fixtures import (
+    post_menu,
+    update_menu,
+    saved_data,
+)
 
 
 @pytest.mark.asyncio
@@ -13,8 +17,8 @@ async def test_get_empty_menus(async_client: AsyncClient):
         "/api/v1/menus/",
     )
 
-    assert response.status_code == 200
-    assert response.json() == []
+    assert response.status_code == 200, "Статус ответа не 200"
+    assert response.json() == [], "В ответе не пустой список"
 
 
 @pytest.mark.asyncio
@@ -28,9 +32,18 @@ async def test_add_menu(
         json=post_menu,
     )
 
-    assert response.status_code == 201
-    assert response.json()["title"] == post_menu["title"]
-    assert response.json()["description"] == post_menu["description"]
+    assert response.status_code == 201, "Статус ответа не 201"
+    assert "id" in response.json(), "В ответе отсутствует id"
+    assert "title" in response.json(), "В ответе отсутствует title"
+    assert "description" in response.json(), "В ответе отсутствует description"
+    assert "submenus_count" in response.json(), "В ответе отсутствует submenus_count"
+    assert "dishes_count" in response.json(), "В ответе отсутствует dishes_count"
+    assert (
+        response.json()["title"] == post_menu["title"]
+    ), "Название не соответствует ожидаемому"
+    assert (
+        response.json()["description"] == post_menu["description"]
+    ), "Описание не соответствует ожидаемому"
 
     menu_id = response.json()["id"]
     assert menu_id is not None
@@ -45,8 +58,8 @@ async def test_get_menus(async_client: AsyncClient):
         "/api/v1/menus/",
     )
 
-    assert response.status_code == 200
-    assert response.json() != []
+    assert response.status_code == 200, "Статус ответа не 200"
+    assert response.json() != [], "В ответе пустой список"
 
 
 @pytest.mark.asyncio
@@ -60,7 +73,63 @@ async def test_get_menu_by_id(
         params={"id": menu["id"]},
     )
 
-    assert response.status_code == 200
-    assert response.json()[0]["id"] == menu["id"]
-    assert response.json()[0]["title"] == menu["title"]
-    assert response.json()[0]["description"] == menu["description"]
+    assert response.status_code == 200, "Статус ответа не 200"
+    assert (
+        response.json()[0]["id"] == menu["id"]
+    ), "Идентификатор не соответствует ожидаемому"
+    assert (
+        response.json()[0]["title"] == menu["title"]
+    ), "Название не соответствует ожидаемому"
+    assert (
+        response.json()[0]["description"] == menu["description"]
+    ), "Описание не соответствует ожидаемому"
+    assert (
+        response.json()[0]["submenus_count"] == menu["submenus_count"]
+    ), "Количество подменю не соответствует ожидаемому"
+    assert (
+        response.json()[0]["dishes_count"] == menu["dishes_count"]
+    ), "Количество блюд не соответствует ожидаемому"
+
+
+@pytest.mark.asyncio
+async def test_update_menu_partial(
+    update_menu: dict[str, str],
+    saved_data: dict[str, Any],
+    async_client: AsyncClient,
+):
+    menu = saved_data["menu"]
+    url = f"/api/v1/menus/{menu['id']}"
+    response = await async_client.patch(
+        url,
+        json=update_menu,
+    )
+
+    assert response.status_code == 200, "Статус ответа не 200"
+    assert (
+        response.json()["title"] == update_menu["title"]
+    ), "Название не соответствует ожидаемому"
+    assert (
+        response.json()["description"] == update_menu["description"]
+    ), "Описание не соответствует ожидаемому"
+    assert "id" in response.json(), "В ответе отсутствует id"
+    assert "title" in response.json(), "В ответе отсутствует title"
+    assert "description" in response.json(), "В ответе отсутствует description"
+    assert "submenus_count" in response.json(), "В ответе отсутствует submenus_count"
+    assert "dishes_count" in response.json(), "В ответе отсутствует dishes_count"
+
+
+# @pytest.mark.asyncio
+# async def test_delete_menu(
+#     saved_data: dict[str, Any],
+#     async_client: AsyncClient,
+# ):
+#     menu = saved_data["menu"]
+#     url = f"/api/v1/menus/{menu['id']}"
+#     print(url)
+#     response = await async_client.delete(url)
+#
+#     print(response)
+#     # assert response.status_code == 200
+#     # assert response.json()[0]["id"] == menu["id"]
+#     # assert response.json()[0]["title"] == menu["title"]
+#     # assert response.json()[0]["description"] == menu["description"]
