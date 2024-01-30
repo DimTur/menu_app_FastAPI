fe6491c81879962898e6502dbda30a92ba4bd39f
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-01-30 21:51:55 +0300

    add consistent tests for dishes endpoints

---
+++ tests/dishes/fixtures.py
@@ -0,0 +1,90 @@
+from typing import Any
+
+import pytest
+from httpx import AsyncClient
+from sqlalchemy import select, insert
+
+from core.models import db_helper, Dish
+from tests.menus.fixtures import test_add_and_get_one_menu
+from tests.submenus.fixtures import test_add_and_get_one_submenu
+
+
+@pytest.fixture
+async def get_empty_dishes(async_client: AsyncClient):
+    session = db_helper.get_scoped_session()
+    query = select(Dish)
+    result = await session.execute(query)
+    dishes = result.all()
+    await session.close()
+    return dishes
+
+
+@pytest.fixture
+async def post_dish() -> dict[str, Any]:
+    return {
+        "title": "DISH1",
+        "description": "DISH1 DISH1 DISH1 DISH1 DISH1",
+        "price": 20.4,
+    }
+
+
+@pytest.fixture
+async def test_add_two_dishes(test_add_and_get_one_submenu):
+    submenu = test_add_and_get_one_submenu[0][0]
+    dishes = [
+        {
+            "title": "DISH1",
+            "description": "DISH1DISH1DISH1DISH1",
+            "submenu_id": submenu.id,
+            "price": 10.88,
+        },
+        {
+            "title": "DISH2",
+            "description": "DISH2DISH2DISH2DISH2",
+            "submenu_id": submenu.id,
+            "price": 15.99,
+        },
+    ]
+
+    session = db_helper.get_scoped_session()
+    for dish in dishes:
+        stmt = insert(Dish).values(**dish)
+        await session.execute(stmt)
+        await session.commit()
+
+    query = select(Dish)
+    result = await session.execute(query)
+    dishes = result.all()
+    await session.close()
+    return dishes
+
+
+@pytest.fixture
+async def test_add_and_get_one_dish(test_add_and_get_one_submenu):
+    submenu = test_add_and_get_one_submenu[0][0]
+
+    session = db_helper.get_scoped_session()
+
+    stmt = insert(Dish).values(
+        title="DISH1",
+        description="DISH1DISH1DISH1DISH1",
+        submenu_id=submenu.id,
+        price=10.88,
+    )
+    await session.execute(stmt)
+    await session.commit()
+
+    query = select(Dish)
+    result = await session.execute(query)
+    dishes = result.all()
+    await session.close()
+    return dishes
+
+
+@pytest.fixture
+async def update_dish() -> dict[str, Any]:
+    return {
+        "title": "NEW DISH",
+        "description": "NEW DISH NEW DISH NEW DISH NEW DISH",
+        "price": 40,
+    }
--- tests/dishes/test_03_dishes.py
+++ tests/dishes/test_03_dishes.py
@@ -4,97 +4,43 @@ from typing import Any
 import pytest
 from httpx import AsyncClient
 
+from tests.menus.fixtures import test_add_and_get_one_menu
+from tests.submenus.fixtures import test_add_and_get_one_submenu
 from fixtures import (
-    post_menu,
-    saved_data,
-    post_submenu,
+    get_empty_dishes,
     post_dish,
+    test_add_two_dishes,
+    test_add_and_get_one_dish,
     update_dish,
 )
 
 
 @pytest.mark.asyncio
-async def test_add_menu(
-    post_menu: dict[str, str],
-    saved_data: dict[str, Any],
-    async_client: AsyncClient,
-):
-    response = await async_client.post(
-        "/api/v1/menus/",
-        json=post_menu,
-    )
-
-    assert response.status_code == 201, "Статус ответа не 201"
-    assert "id" in response.json(), "В ответе отсутствует id"
-    assert "title" in response.json(), "В ответе отсутствует title"
-    assert "description" in response.json(), "В ответе отсутствует description"
-    assert "submenus_count" in response.json(), "В ответе отсутствует submenus_count"
-    assert "dishes_count" in response.json(), "В ответе отсутствует dishes_count"
-    assert (
-        response.json()["title"] == post_menu["title"]
-    ), "Название не соответствует ожидаемому"
-    assert (
-        response.json()["description"] == post_menu["description"]
-    ), "Описание не соответствует ожидаемому"
-
-    menu_id = response.json()["id"]
-    assert menu_id is not None
-    assert isinstance(menu_id, str)
-
-    saved_data["menu"] = response.json()
-
-
-@pytest.mark.asyncio
-async def test_add_submenu(
-    post_submenu: dict[str, Any],
-    saved_data: dict[str, Any],
-    async_client: AsyncClient,
-):
-    menu = saved_data["menu"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/"
-    response = await async_client.post(
-        url,
-        json=post_submenu,
-    )
-
-    assert response.status_code == 201, "Статус ответа не 201"
-    assert "id" in response.json(), "В ответе отсутствует id"
-    assert "title" in response.json(), "В ответе отсутствует title"
-    assert "description" in response.json(), "В ответе отсутствует description"
-    assert "dishes_count" in response.json(), "В ответе отсутствует dishes_count"
-    assert (
-        response.json()["title"] == post_submenu["title"]
-    ), "Название не соответствует ожидаемому"
-    assert (
-        response.json()["description"] == post_submenu["description"]
-    ), "Описание не соответствует ожидаемому"
-
-    saved_data["submenu"] = response.json()
-
-
-@pytest.mark.asyncio
 async def test_get_empty_dishes(
-    saved_data: dict[str, Any],
+    test_add_and_get_one_menu,
+    test_add_and_get_one_submenu,
+    get_empty_dishes,
     async_client: AsyncClient,
 ):
-    menu = saved_data["menu"]
-    submenu = saved_data["submenu"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/{submenu['id']}/dishes/"
+    menu = test_add_and_get_one_menu[0][0]
+    submenu = test_add_and_get_one_submenu[0][0]
+    url = f"/api/v1/menus/{menu.id}/submenus/{submenu.id}/dishes/"
     response = await async_client.get(url)
 
     assert response.status_code == 200, "Статус ответа не 200"
-    assert response.json() == [], "В ответе не пустой список"
+    assert response.json() == get_empty_dishes, "В ответе не пустой список"
 
 
 @pytest.mark.asyncio
 async def test_add_dish(
     post_dish: dict[str, str, Any],
-    saved_data: dict[str, Any],
+    test_add_and_get_one_menu,
+    test_add_and_get_one_submenu,
     async_client: AsyncClient,
 ):
-    menu = saved_data["menu"]
-    submenu = saved_data["submenu"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/{submenu['id']}/dishes/"
+    menu = test_add_and_get_one_menu[0][0]
+    submenu = test_add_and_get_one_submenu[0][0]
+    url = f"/api/v1/menus/{menu.id}/submenus/{submenu.id}/dishes/"
     response = await async_client.post(
         url,
         json=post_dish,
@@ -115,17 +61,16 @@ async def test_add_dish(
         Decimal(post_dish["price"]).quantize(Decimal("0.00"))
     ), "Цена не соответствует ожидаемой"
 
-    saved_data["dish"] = response.json()
 
-
-@pytest.mark.asyncio
+@pytest.mark.usefixtures("test_add_two_dishes")
 async def test_get_list_dishes(
-    saved_data: dict[str, Any],
+    test_add_and_get_one_menu,
+    test_add_and_get_one_submenu,
     async_client: AsyncClient,
 ):
-    menu = saved_data["menu"]
-    submenu = saved_data["submenu"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/{submenu['id']}/dishes/"
+    menu = test_add_and_get_one_menu[0][0]
+    submenu = test_add_and_get_one_submenu[0][0]
+    url = f"/api/v1/menus/{menu.id}/submenus/{submenu.id}/dishes/"
     response = await async_client.get(url)
 
     assert response.status_code == 200, "Статус ответа не 200"
@@ -134,40 +79,44 @@ async def test_get_list_dishes(
 
 @pytest.mark.asyncio
 async def test_get_dish_by_id(
-    saved_data: dict[str, Any],
+    test_add_and_get_one_menu,
+    test_add_and_get_one_submenu,
+    test_add_and_get_one_dish,
     async_client: AsyncClient,
 ):
-    menu = saved_data["menu"]
-    submenu = saved_data["submenu"]
-    dish = saved_data["dish"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/{submenu['id']}/dishes/{dish['id']}"
+    menu = test_add_and_get_one_menu[0][0]
+    submenu = test_add_and_get_one_submenu[0][0]
+    dish = test_add_and_get_one_dish[0][0]
+    url = f"/api/v1/menus/{menu.id}/submenus/{submenu.id}/dishes/{dish.id}"
     response = await async_client.get(url)
 
     assert response.status_code == 200, "Статус ответа не 200"
-    assert (
-        response.json()["id"] == dish["id"]
+    assert response.json()["id"] == str(
+        dish.id
     ), "Идентификатор не соответствует ожидаемому"
     assert (
-        response.json()["title"] == dish["title"]
+        response.json()["title"] == dish.title
     ), "Название не соответствует ожидаемому"
     assert (
-        response.json()["description"] == dish["description"]
+        response.json()["description"] == dish.description
     ), "Описание не соответствует ожидаемому"
     assert response.json()["price"] == str(
-        Decimal(dish["price"]).quantize(Decimal("0.00"))
+        Decimal(dish.price).quantize(Decimal("0.00"))
     ), "Цена не соответствует ожидаемой"
 
 
 @pytest.mark.asyncio
 async def test_update_dish_partial(
     update_dish: dict[str, str],
-    saved_data: dict[str, Any],
+    test_add_and_get_one_menu,
+    test_add_and_get_one_submenu,
+    test_add_and_get_one_dish,
     async_client: AsyncClient,
 ):
-    menu = saved_data["menu"]
-    submenu = saved_data["submenu"]
-    dish = saved_data["dish"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/{submenu['id']}/dishes/{dish['id']}"
+    menu = test_add_and_get_one_menu[0][0]
+    submenu = test_add_and_get_one_submenu[0][0]
+    dish = test_add_and_get_one_dish[0][0]
+    url = f"/api/v1/menus/{menu.id}/submenus/{submenu.id}/dishes/{dish.id}"
     response = await async_client.patch(
         url,
         json=update_dish,
@@ -188,126 +137,19 @@ async def test_update_dish_partial(
     assert "description" in response.json(), "В ответе отсутствует description"
     assert "price" in response.json(), "В ответе отсутствует price"
 
-    saved_data["dish"] = response.json()
-
-
-@pytest.mark.asyncio
-async def test_get_updated_dish_by_id(
-    saved_data: dict[str, Any],
-    async_client: AsyncClient,
-):
-    menu = saved_data["menu"]
-    submenu = saved_data["submenu"]
-    dish = saved_data["dish"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/{submenu['id']}/dishes/{dish['id']}"
-    response = await async_client.get(url)
-
-    assert response.status_code == 200, "Статус ответа не 200"
-    assert (
-        response.json()["id"] == dish["id"]
-    ), "Идентификатор не соответствует ожидаемому"
-    assert (
-        response.json()["title"] == dish["title"]
-    ), "Название не соответствует ожидаемому"
-    assert (
-        response.json()["description"] == dish["description"]
-    ), "Описание не соответствует ожидаемому"
-    assert response.json()["price"] == str(
-        Decimal(dish["price"]).quantize(Decimal("0.00"))
-    ), "Цена не соответствует ожидаемой"
-
 
 @pytest.mark.asyncio
 async def test_delete_dish(
-    saved_data: dict[str, Any],
+    test_add_and_get_one_menu,
+    test_add_and_get_one_submenu,
+    test_add_and_get_one_dish,
     async_client: AsyncClient,
 ):
-    menu = saved_data["menu"]
-    submenu = saved_data["submenu"]
-    dish = saved_data["dish"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/{submenu['id']}/dishes/{dish['id']}"
+    menu = test_add_and_get_one_menu[0][0]
+    submenu = test_add_and_get_one_submenu[0][0]
+    dish = test_add_and_get_one_dish[0][0]
+    url = f"/api/v1/menus/{menu.id}/submenus/{submenu.id}/dishes/{dish.id}"
     response = await async_client.delete(url)
 
     assert response.status_code == 200, "Статус ответа не 200"
     assert response.json() is None, "Сообщение об удалении не соответствует ожидаемому"
-
-
-@pytest.mark.asyncio
-async def test_get_new_empty_dishes(
-    saved_data: dict[str, Any],
-    async_client: AsyncClient,
-):
-    menu = saved_data["menu"]
-    submenu = saved_data["submenu"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/{submenu['id']}/dishes/"
-    response = await async_client.get(url)
-
-    assert response.status_code == 200, "Статус ответа не 200"
-    assert response.json() == [], "В ответе не пустой список"
-
-
-@pytest.mark.asyncio
-async def test_get_deleted_dish_by_id(
-    saved_data: dict[str, Any],
-    async_client: AsyncClient,
-):
-    menu = saved_data["menu"]
-    submenu = saved_data["submenu"]
-    dish = saved_data["dish"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/{submenu['id']}/dishes/{dish['id']}"
-    response = await async_client.get(url)
-
-    assert response.status_code == 404, "Статус ответа не 404"
-    assert (
-        response.json()["detail"] == "dish not found"
-    ), "Сообщение об ошибке не соответствует ожидаемому"
-
-
-@pytest.mark.asyncio
-async def test_delete_submenu(
-    saved_data: dict[str, Any],
-    async_client: AsyncClient,
-):
-    menu = saved_data["menu"]
-    submenu = saved_data["submenu"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/{submenu['id']}"
-    response = await async_client.delete(url)
-
-    assert response.status_code == 200, "Статус ответа не 200"
-    assert response.json() is None, "Сообщение об удалении не соответствует ожидаемому"
-
-
-@pytest.mark.asyncio
-async def test_get_new_empty_submenus(
-    saved_data: dict[str, Any],
-    async_client: AsyncClient,
-):
-    menu = saved_data["menu"]
-    url = f"/api/v1/menus/{menu['id']}/submenus/"
-    response = await async_client.get(url)
-
-    assert response.status_code == 200, "Статус ответа не 200"
-    assert response.json() == [], "В ответе не пустой список"
-
-
-@pytest.mark.asyncio
-async def test_delete_menu(
-    saved_data: dict[str, Any],
-    async_client: AsyncClient,
-):
-    menu = saved_data["menu"]
-    url = f"/api/v1/menus/{menu['id']}"
-    response = await async_client.delete(url)
-
-    assert response.status_code == 200, "Статус ответа не 200"
-    assert response.json() is None, "Сообщение об удалении не соответствует ожидаемому"
-
-
-@pytest.mark.asyncio
-async def test_get_empty_menus(async_client: AsyncClient):
-    response = await async_client.get(
-        "/api/v1/menus/",
-    )
-
-    assert response.status_code == 200, "Статус ответа не 200"
-    assert response.json() == [], "В ответе не пустой список"
