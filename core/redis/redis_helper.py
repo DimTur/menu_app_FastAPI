749e1c3c5de354db8c5d8008673d97364daf017d
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-02-01 15:44:07 +0300

    rebild redis client from sync to async

--- core/redis/redis_helper.py
+++ core/redis/redis_helper.py
@@ -1,8 +1,9 @@
 import os
 
-import redis
+import redis.asyncio as redis
 from dotenv import load_dotenv
 from pydantic import BaseConfig
+from redis.asyncio import ConnectionPool, Redis
 
 load_dotenv()
 
@@ -14,9 +15,38 @@ class GlobalConfig(BaseConfig):
 
 settings = GlobalConfig()
 
+REDIS_URL = f"redis://{GlobalConfig.redis_server}:{GlobalConfig.redis_port}"
 
-def cache():
-    return redis.Redis(
-        host=GlobalConfig.redis_server,
-        port=GlobalConfig.redis_port,
-    )
+
+async def cache():
+    async with redis.from_url(REDIS_URL) as client:
+        try:
+            yield client
+        finally:
+            await client.aclose()
+
+
+# REDIS_URL = f"redis://{GlobalConfig.redis_server}:{GlobalConfig.redis_port}/0"
+#
+#
+# def create_redis():
+#     return ConnectionPool.from_url(REDIS_URL)
+#
+#
+# pool = create_redis()
+#
+#
+# def cache():
+#     return Redis(connection_pool=pool)
+
+
+# async def cache():
+#     return redis.Redis(
+#         host=GlobalConfig.redis_server,
+#         port=GlobalConfig.redis_port,
+#     )
+# async def cache():
+#     return await redis.from_url(
+#         host=GlobalConfig.redis_server,
+#         port=GlobalConfig.redis_port,
+#     )
