5ca361b869674d8ae67801228a65f960677e400d
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-02-01 11:24:18 +0300

    add cache to menus

--- api_v1/menus/dependencies.py
+++ api_v1/menus/dependencies.py
@@ -1,4 +1,5 @@
 import uuid
+import pickle
 from typing import Annotated
 
 from fastapi import (
@@ -10,19 +11,30 @@ from fastapi import (
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper, Menu
+from core.redis import cache
 
 from . import crud
 
 
+class EntityDoesNotExist(Exception):
+    """Raised when entity was not found in database."""
+
+
 async def menu_by_id(
     menu_id: Annotated[uuid.UUID, Path],
+    redis_client: cache = Depends(cache),
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ) -> Menu:
-    menu = await crud.get_menu_by_id(session=session, menu_id=menu_id)
-    if menu is not None:
+    if (cached_menu := redis_client.get(f"menu_{menu_id}")) is not None:
+        return pickle.loads(cached_menu)
+    try:
+        menu = await crud.get_menu_by_id(session=session, menu_id=menu_id)
+        redis_client.set(f"menu_{menu_id}", pickle.dumps(menu))
+
         return menu
 
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"menu not found",
-    )
+    except EntityDoesNotExist:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"menu not found",
+        )
--- api_v1/menus/views.py
+++ api_v1/menus/views.py
@@ -4,46 +4,18 @@ from fastapi import (
     APIRouter,
     Depends,
     status,
-    HTTPException,
-    BackgroundTasks,
 )
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
 from . import crud
-from .cache_crud import MenuService
 
 from .dependencies import menu_by_id
 from .schemas import Menu, MenuCreate, MenuUpdatePartial
 
-from core.redis.redis_helper import cache
+from core.redis import cache
 
 router = APIRouter(tags=["Menus"])
-
-
-async def get_list_menus_cache(redis_client, key):
-    if (cached_menus := redis_client.get(key)) is not None:
-        return pickle.loads(cached_menus)
-    return None
-
-
-async def set_list_menus_cache(redis_client, key, data):
-    await redis_client.set(key, pickle.dumps(data))
-
-
-async def get_all_menus(
-    redis_client,
-    session,
-    cache_key,
-    crud_func,
-):
-    cached_data = await get_list_menus_cache(redis_client, cache_key)
-    if cached_data is not None:
-        return cached_data
-
-    data = await crud_func(session=session)
-    await set_list_menus_cache(redis_client, cache_key, data)
-    return data
 
 
 @router.get("/", response_model=list[Menu])
@@ -51,35 +23,12 @@ async def get_menus(
     redis_client: cache = Depends(cache),
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
-    cache_key = "list_menus"
-    menus = await get_all_menus(
-        redis_client=redis_client,
-        session=session,
-        cache_key=cache_key,
-        crud_func=crud.get_menus,
-    )
+    if (cached_menus := redis_client.get("menus")) is not None:
+        return pickle.loads(cached_menus)
+
+    menus = await crud.get_menus(session=session)
+    redis_client.set("menus", pickle.dumps(menus))
     return menus
-
-
-# @router.get("/", response_model=list[Menu])
-# async def get_menus(
-#     redis_client: cache = Depends(cache),
-#     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
-# ):
-#     if (cached_menus := redis_client.get("menus")) is not None:
-#         return pickle.loads(cached_menus)
-#
-#     menus = await crud.get_menus(session=session)
-#     redis_client.set("menus", pickle.dumps(menus))
-#     return menus
-
-
-# @router.get("/", response_model=list[Menu])
-# async def get_menus(
-#     background_tasks: BackgroundTasks,
-#     repo: MenuService = Depends(),
-# ):
-#     return await repo.get_all_menus(background_tasks=background_tasks)
 
 
 @router.post(
@@ -89,8 +38,10 @@ async def get_menus(
 )
 async def create_menu(
     menu_in: MenuCreate,
+    redis_client: cache = Depends(cache),
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
+    redis_client.delete("menus")
     return await crud.create_menu(session=session, menu_in=menu_in)
 
 
@@ -105,25 +56,30 @@ async def get_menu_by_id(
 async def update_menu_partial(
     menu_update: MenuUpdatePartial,
     menu: Menu = Depends(menu_by_id),
+    redis_client: cache = Depends(cache),
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
-    return await crud.update_menu(
+    updated_menu = await crud.update_menu(
         session=session,
         menu=menu,
         menu_update=menu_update,
         partial=True,
     )
+    redis_client.delete(f"menu_{menu.id}")
+
+    return updated_menu
 
 
-@router.delete(
-    "/{menu_id}",
-    # status_code=status.HTTP_204_NO_CONTENT,
-)
+@router.delete("/{menu_id}")
 async def delete_menu(
     menu: Menu = Depends(menu_by_id),
+    redis_client: cache = Depends(cache),
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ) -> None:
     await crud.delete_menu(
         session=session,
         menu=menu,
     )
+    redis_client.delete(f"menu_{menu.id}")
+    menus = await crud.get_menus(session=session)
+    redis_client.set("menus", pickle.dumps(menus))
