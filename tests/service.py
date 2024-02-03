68204033318bb33c821fc78f1ef446072cf39ef0
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-02-04 00:39:10 +0300

    initialized func reverse for tests

---
+++ tests/service.py
@@ -0,0 +1,19 @@
+from typing import Callable
+
+from main import app
+
+
+def get_routes() -> dict[str, str]:
+    """Получение словаря с маршрутами приложений"""
+    routes = {}
+    for route in app.routes:
+        routes[route.endpoint.__name__] = route.path
+    # print(routes)
+    return routes
+
+
+def reverse(foo: Callable, routes: dict[str, str] = get_routes(), **kwargs) -> str:
+    """Получение url адресу"""
+    path = routes[foo.__name__]
+    print(path.format(**kwargs))
+    return path.format(**kwargs)
